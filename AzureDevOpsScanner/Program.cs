c209ce9ec4877504f2d09dc05d19c6736ccc8432
Simon Wu <siw@microsoft.com>
2018-11-20 15:49:39 +0800

    apply new strategy

--- AzureDevOpsScanner/Program.cs
+++ AzureDevOpsScanner/Program.cs
@@ -42,6 +42,11 @@ namespace AzureDevOpsScanner
 
             Dictionary<TeamProjectReference, IEnumerable<WebApiTeam>> results = new Dictionary<TeamProjectReference, IEnumerable<WebApiTeam>>();
 
+            IList<RepoBranchStatus> result = new List<RepoBranchStatus>();
+
+            int count = 0;
+            List<GitBranchStats> branches = new List<GitBranchStats>();
+
             // Iterate over the returned projects
             foreach (var project in projects)
             {
@@ -67,91 +72,77 @@ namespace AzureDevOpsScanner
                     }
                 }
 
-                Console.WriteLine("| Repository Name | Branch | Has README.md | Has Owner | Has Policy | Policies |");
-                Console.WriteLine("| :--- | :--- | :--- | :--- | :--- | :--- |");
                 foreach (var repo in gitClient.GetRepositoriesAsync(project.Id).Result)
                 {
-                    List<GitBranchStats> branches = new List<GitBranchStats>();
                     try
                     {
                         branches = gitClient.GetBranchesAsync(repo.Id).Result;
                     }
                     catch
                     {
-
                     }
 
-                    foreach (var branch in branches)
+                    foreach (var branch in branches.Where(branch => branch.IsBaseVersion))
                     {
-                        if (branch.IsBaseVersion)
+                        Console.WriteLine($"({++count}) {repo.Name} - {branch.Name}");
+                        var repoBranchStatus = new RepoBranchStatus()
                         {
-                            Console.Write($"| {repo.Name} | {branch.Name}");
-                            try
+                            RepoName = repo.Name,
+                            Branch = branch.Name
+                        };
+
+                        try
+                        {
+                            var readme = gitClient.GetItemAsync(repo.Id, "README.md", includeContent: true).Result;
+                            if (!readme.Content.Contains("TODO: Give a short introduction of your project."))
                             {
-                                var readme = gitClient.GetItemAsync(repo.Id, "README.md", includeContent: true).Result;
-                                if (!readme.Content.Contains("TODO: Give a short introduction of your project."))
-                                {
-                                    Console.Write($" | True");
-                                }
-                                else
-                                {
-                                    Console.Write($" | False");
-                                }
+                                repoBranchStatus.HasReadMe = true;
+                            }
                                 
-                                if (readme.Content.ToLower().Contains("owner") || readme.Content.ToLower().Contains("contact")
-                                    || EmailRegex.IsMatch(readme.Content))
-                                {
-                                    Console.Write($" | True");
-                                }
-                                else
-                                {
-                                    Console.Write($" | False");
-                                }
-                            }
-                            catch
+                            if (readme.Content.ToLower().Contains("owner") || readme.Content.ToLower().Contains("contact")
+                                || EmailRegex.IsMatch(readme.Content))
                             {
-                                Console.Write($" | False | False");
+                                repoBranchStatus.HasOwner = true;
                             }
-
-                            var repositoryId = repo.Id.ToString();
-                            var repoBranch = $"{repositoryId}-{branch.Name}";
-                            if (policyDict.ContainsKey(repoBranch))
-                            {
-                                Console.Write($" | True | ");
-                                var policies = policyDict[repoBranch];
-                                Console.Write(string.Join("<br><br>", policies.Select(p =>
-                                {
-                                    if (p.Type.DisplayName == "Minimum number of reviewers")
-                                    {
-                                        return $"{p.Type.DisplayName}: {p.Settings["minimumApproverCount"]} {(p.Settings["creatorVoteCounts"].ToString() == "True" ? "(creatorVoteCounts)" : "")}";
-                                    }
-                                    else if (p.Type.DisplayName == "Require a merge strategy")
-                                    {
-                                        return $"{p.Type.DisplayName}: {((p.Settings["useSquashMerge"].ToString() == "True") ? "Squash" : "Non-Squash")}";
-                                    }
-                                    else if (p.Type.DisplayName == "Build")
-                                    {
-                                        return "Require a successful build before updating protected refs";
-                                    }
-                                    else if (p.Type.DisplayName == "Status")
-                                    {
-                                        return "Require a successful status to be posted before updating protected refs";
-                                    }
-                                    else if (p.Type.DisplayName == "Comment requirements")
-                                    {
-                                        return "Check if the pull request has any active comments";
-                                    }
-
-                                    return p.Type.DisplayName;
-                                })));
-                            }
-                            else
-                            {
-                                Console.Write($" | False | |");
-                            }
-
-                            Console.WriteLine(" |");
                         }
+                        catch
+                        {
+                        }
+
+                        var repositoryId = repo.Id.ToString();
+                        var repoBranch = $"{repositoryId}-{branch.Name}";
+                        if (policyDict.ContainsKey(repoBranch))
+                        {
+                            repoBranchStatus.HasPolicy = true;
+                            var policies = policyDict[repoBranch];
+                            repoBranchStatus.Policies = policies.Select(p =>
+                            {
+                                if (p.Type.DisplayName == "Minimum number of reviewers")
+                                {
+                                    return $"{p.Type.DisplayName}: {p.Settings["minimumApproverCount"]} {(p.Settings["creatorVoteCounts"].ToString() == "True" ? "(creatorVoteCounts)" : "")}";
+                                }
+                                else if (p.Type.DisplayName == "Require a merge strategy")
+                                {
+                                    return $"{p.Type.DisplayName}: {((p.Settings["useSquashMerge"].ToString() == "True") ? "Squash" : "Non-Squash")}";
+                                }
+                                else if (p.Type.DisplayName == "Build")
+                                {
+                                    return "Require a successful build before updating protected refs";
+                                }
+                                else if (p.Type.DisplayName == "Status")
+                                {
+                                    return "Require a successful status to be posted before updating protected refs";
+                                }
+                                else if (p.Type.DisplayName == "Comment requirements")
+                                {
+                                    return "Check if the pull request has any active comments";
+                                }
+
+                                return p.Type.DisplayName;
+                            }).ToList();
+                        }
+
+                        result.Add(repoBranchStatus);
                     }
                 }
             }
@@ -159,6 +150,10 @@ namespace AzureDevOpsScanner
             if (projects.Count() == 0)
             {
                 Console.WriteLine("No projects found.");
+            }
+            else
+            {
+                OutputResult(result);
             }
 
             //Display reults in console
@@ -180,6 +175,16 @@ namespace AzureDevOpsScanner
             */
 
             Console.ReadLine();
+        }
+
+        public static void OutputResult(IList<RepoBranchStatus> repoBranchStatuses)
+        {
+            Console.WriteLine("| Repository Name | Branch | Has README.md | Has Owner | Has Policy | Policies |");
+            Console.WriteLine("| :--- | :--- | :--- | :--- | :--- | :--- |");
+            foreach (var repoBranchStatus in repoBranchStatuses.OrderBy(repoBranchStatus => repoBranchStatus.RepoName))
+            {
+                Console.WriteLine($"| {repoBranchStatus.RepoName} | {repoBranchStatus.Branch} | {repoBranchStatus.HasReadMe.ToString()} | {repoBranchStatus.HasOwner.ToString()} | {repoBranchStatus.HasPolicy.ToString()} | {string.Join("<br><br>", repoBranchStatus.Policies)} |");
+            }
         }
     }
 }
---
+++ AzureDevOpsScanner/RepoBranchStatus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDevOpsScanner
+{
+    public class RepoBranchStatus
+    {
+        public RepoBranchStatus()
+        {
+            Policies = new List<string>();
+        }
+
+        public string RepoName { get; set; }
+
+        public string Branch { get; set; }
+
+        public bool HasReadMe { get; set; }
+
+        public bool HasOwner { get; set; }
+
+        public bool HasPolicy { get; set; }
+
+        public IList<string> Policies { get; set; }
+    }
+}
